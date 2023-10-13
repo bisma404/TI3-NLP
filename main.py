936c1727e37769d4816b0bc466a71be2e9de1c89
Bisma Nurrauf <66913806+bisma404@users.noreply.github.com>
2023-10-13 10:51:20 +0000

    Menamatkan code

--- main.py
+++ main.py
@@ -15,7 +15,76 @@ dan atur parameter MAX_COST untuk setiap algoritma sebesar 2 ketika memanggil
 fungsi untuk pengambilan kandidat.
 """
 def main():
-    return None
+
+    l_trie = LevenshteinTrie('./bahasa-indonesia-dictionary.txt')
+    dl_trie = DamerauLevenshteinTrie('./bahasa-indonesia-dictionary.txt')
+    l_dict = LevenshteinDict('./bahasa-indonesia-dictionary.txt')
+    dl_dict = DamerauLevenshteinDict('./bahasa-indonesia-dictionary.txt')
+
+    performance = Performance()
+
+    # l_trie
+    l_trie_start = time.perf_counter()
+    bma_l_trie = performance.best_match_acc(l_trie)
+    cm_l_trie = performance.candidate_match(l_trie)
+    l_trie_end = time.perf_counter()
+    l_trie_time_elapsed = l_trie_end - l_trie_start
+                    
+    # dl_trie
+    dl_trie_start = time.perf_counter()
+    bma_dl_trie = performance.best_match_acc(dl_trie)
+    cm_dl_trie = performance.candidate_match(dl_trie)
+    dl_trie_end = time.perf_counter()
+    dl_trie_time_elapsed = dl_trie_end - dl_trie_start
+
+    # l_dict
+    l_dict_start = time.perf_counter()
+    bma_l_dict = performance.best_match_acc(l_dict)
+    cm_l_dict = performance.candidate_match(l_dict)
+    l_dict_end = time.perf_counter()
+    l_dict_time_elapsed = l_dict_end - l_dict_start
+
+    # dl_dict
+    dl_dict_start = time.perf_counter()
+    bma_dl_dict = performance.best_match_acc(dl_dict)
+    cm_dl_dict = performance.candidate_match(dl_dict)
+    dl_dict_end = time.perf_counter()
+    dl_dict_time_elapsed = dl_dict_end - dl_dict_start
+
+    metrics = '''
+    Bisma Nurrauf - 2006525596
+    ----------lev_trie----------
+    best accuracy:  {}
+    candidate accuracy:  {}
+    total_time:  {}
+    ----------dalev_trie----------
+    best accuracy:  {}
+    candidate accuracy:  {}
+    total_time:  {}
+    ----------lev_dict----------
+    best accuracy:  {}
+    candidate accuracy:  {}
+    total_time:  {}
+    ----------dalev_dict----------
+    best accuracy:  {}
+    candidate accuracy:  {}
+    total_time:  {}
+    '''.format(
+        bma_l_trie,
+        cm_l_trie,
+        l_trie_time_elapsed,
+        bma_dl_trie,
+        cm_dl_trie,
+        dl_trie_time_elapsed,
+        bma_l_dict,
+        cm_l_dict,
+        l_dict_time_elapsed,
+        bma_dl_dict,
+        cm_dl_dict,
+        dl_dict_time_elapsed
+    )
+
+    print(metrics)
 
 if __name__ == '__main__':
     main()
--- performance.py
+++ performance.py
@@ -4,4 +4,32 @@ TODO: Gunakan file performance.py ini untuk membuat class Performance yang
 nantinya akan digunakan untuk menghitung akurasi (Best Match Accuracy & Candidate Match) 
 dan run-time
 """
-    
+
+import json
+import time
+
+class Performance():
+
+    def __init__(self):
+        f = open('./saltik_200.json')
+        self.data = json.load(f)
+
+    def best_match_acc(self, match):
+        self.match = match
+        my_dict = self.data
+
+        M = sum(1 for word, error in my_dict.items() for typo in error if self.match.get_candidates(typo['typo'], 2)[0] == word)
+        N = sum(1 for word in my_dict for error in my_dict[word])
+
+        acc = M / N * 100
+        return acc
+
+    def candidate_match(self, candidate):
+        self.candidate = candidate
+        my_dict = self.data
+
+        M = sum(1 for word, error in my_dict.items() for typo in error if word in self.candidate.get_candidates(typo['typo'], 2))
+        N = sum(1 for word in my_dict for error in my_dict[word])
+
+        acc = M / N * 100
+        return acc
